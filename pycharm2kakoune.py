73ae2d1444f13f941bfd949437e3fb6f4fb30704
Cyril Dutrieux (cydu) <cydu@odoo.com>
2022-01-31 16:42:01 +0100

    Add snippets and generating script
    The scripts generates the snippets from the pycharm template submodule

---
+++ pycharm2kakoune.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+import re, sys
+
+tree = ET.parse('odoo-pycharm-templates/Odoo.xml')
+root = tree.getroot()
+for template in root:
+    # # import ipdb; ipdb.set_trace()
+    value = template.attrib.get('value')
+    name = template.attrib.get('name')
+    print(name)
+    res = re.findall(r"(\$(.*?)\$)+", value)
+    for i in range(0, len(res)):
+        rr = res[i]
+        value = value.replace(rr[0], '${%s:%s}' % (i, rr[1]))
+
+    try:
+        language = template.find("context").find("option").attrib.get('name')
+        language = language.lower()
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+        pass
+
+    if language not in ['xml', 'python']:
+        language = 'other'
+
+    f = open("%s/%s" % (language, name), "w")
+    f.write(value)
+    f.close()
